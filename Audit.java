b4fb86511c19237b431ce2aa801d43321012c826
hlaffon <helene.laffon@gmail.com>
2016-04-14 12:45:52 +0200

    added service classes

---
+++ Audit.java
@@ -0,0 +1,10 @@
+package com.proxi.service;
+
+public class Audit {
+
+	public boolean verification_solde() {
+		// TODO - implement Audit.verification_solde
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ SimulationCredit.java
@@ -0,0 +1,11 @@
+
+package com.proxi.service;
+
+public class SimulationCredit {
+
+	public void simuler() {
+		// TODO - implement SimulationCredit.simuler
+		throw new UnsupportedOperationException();
+	}
+
+}
